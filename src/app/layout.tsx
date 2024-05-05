ff92a1758daa91a85bf61eb5ff059e393885fdbe
Martin Paucot <contact@martin-paucot.fr>
2024-05-06 00:50:59 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
           <Overlay />
           {children}
         </SettingsProvider>
+        <Analytics />
       </body>
     </html>
   );
